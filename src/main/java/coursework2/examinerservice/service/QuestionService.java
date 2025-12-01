f970e252599241740cd95755291260bee3ab848f
alkol <git@github.com:Omirakov/Homework13.git>
2025-12-01 19:03:59 +0300

    Hard level RESOLVED

--- src/main/java/coursework2/examinerservice/service/QuestionService.java
+++ src/main/java/coursework2/examinerservice/service/QuestionService.java
@@ -1,7 +1,6 @@
 package coursework2.examinerservice.service;
 
 import coursework2.examinerservice.domain.Question;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 
 import java.util.Collection;
 
@@ -9,11 +8,11 @@ public interface QuestionService {
 
     Question add(String question, String answer);
 
-    Question add(Question question) throws HttpRequestMethodNotSupportedException;
+    Question add(Question question);
 
-    Question remove(Question question) throws HttpRequestMethodNotSupportedException;
+    Question remove(Question question);
 
-    Collection<Question> getAll() throws HttpRequestMethodNotSupportedException;
+    Collection<Question> getAll();
 
     Question getRandomQuestion();
 }
